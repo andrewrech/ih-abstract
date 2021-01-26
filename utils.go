d1e539a59637faf34126f012e5865de4d29ec13b
andrewrech <rech@rech.io>
2021-01-26 13:03:43 +0000

    Add channel utility used in main

--- utils.go
+++ utils.go
@@ -32,3 +32,25 @@ func count(counter *int64, descr string, signal chan struct{}) {
 		}
 	}()
 }
+
+// splitCh splits a []string channel into two channels, sending results from the input channel onto both output channels
+func splitCh(in chan []string) (out1 chan []string, out2 chan []string, done chan struct{}) {
+	var buf int64 = 2e7
+
+	out1 = make(chan []string, buf)
+	out2 = make(chan []string, buf)
+	done = make(chan struct{})
+
+	go func() {
+		for l := range in {
+			out1 <- l
+			out2 <- l
+		}
+
+		close(out1)
+		close(out2)
+		done <- struct{}{}
+	}()
+
+	return
+}
